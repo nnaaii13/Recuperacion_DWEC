5ffca9b27e930f1afbbf3c8296aa11a1c3c8feff
Naila <nayeli_das@hotmail.com>
2025-01-09 10:12:13 +0100

    juega la máquina y son X y O
    He añadido el símbolo X, O a los colores y he añadido la función máquina que hace que juegue el ordenador.

--- app.js
+++ app.js
@@ -1,14 +1,47 @@
 let color = "purple"; //definimos el color con el que vamos a empezar
+let turno = "X"; //definimos el turno con el que empezaremos, es decir X u O. En este caso empezamos con X
 
 //función que cambia el color de las casillas del tablero al hacer click
 function cambiarColor(element) {
     element.style.backgroundColor = color; 
+    element.innerHTML = turno;
 
     //un click morado, un click rosa y así sucesivamente
     if (color === "purple") {
         color = "pink";
+        turno = "O"; //cambia el simbolo a O
     } else {
         color = "purple";
+        turno = "X"; //cambia el simbolo a X
+    }
+
+    if (turno === "O") { //si el turno es O, juega la máquina
+        setTimeout(maquina, 200); //el tiempo que tarda la máquina en responder
+    }
+}
+
+function maquina() {
+    const botones = document.querySelectorAll("#tablero button");
+    let casillaElegida = null;
+
+    //busca una casilla vacía
+    for (let i = 0; i < botones.length; i++) {
+        if (botones[i].innerHTML === "") {
+            //elige una casilla vacía
+            if (Math.random() < 1 / (botones.length - i)) { //con esto haremos que seleccione una de las casillas vacías al azar
+                casillaElegida = botones[i];
+            }
+        }
+    }
+
+    //si hay casillas vacías la máquina pone O (y por lo tanto rosa), en una aleatoria
+    if (casillaElegida) {
+        casillaElegida.style.backgroundColor = "pink";
+        casillaElegida.innerHTML = "O";
+
+        //el siguiente turno es morado, X
+        color = "purple";
+        turno = "X";
     }
 }
 
@@ -19,6 +52,7 @@ function reset() {
         element.style.backgroundColor = "";
     });
     color = "purple"; //esto es para que vuelva a comenzar por morado
+    turno = "X"; //vuelve a comenzar por X
 }
 
 //seleccionamos los botones y los recorremos con el foreach, para que cuando hagamos click cambie el color
