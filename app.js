189e4001a02ba4ac1a9d31655f3171a4905e02d4
Naila <nayeli_das@hotmail.com>
2025-01-10 12:33:14 +0100

    mensaje de ganar o perder, array con combinaciones para ganar, no permita seguir jugando despues de ganar o perder
    He implementado el mensaje de si gano o pierdo contra la máquina, he creado un array para guardar las combinaciones posibles para ganar, he creado una variable para que no me permita seguir jugando después de ganar o perder. He actualizado el html con div resultado (que incluye el mensaje) y he configurado el css con parámetros para el mensaje de ganar o perder

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 let color = "purple"; //definimos el color con el que vamos a empezar
 let turno = "X"; //definimos el turno con el que empezaremos, es decir X u O. En este caso empezamos con X y empezamos nosotros en vez del ordenador
+let tablero = true; //esto lo hago para que cuando salte el mensaje de ganar o perder, no permita seguir haciendo click en los botones vacíos.
 
 //función que cambia el color de las casillas del tablero al hacer click
 function cambiarColor(element) {
+
+    if (!tablero) { //para que cuando el juego no esté realizandose no haga nada
+        return;
+    }
 
     //esto lo he implementado para que no pueda modificar mi símbolo, antes podía cambiar mi botón X por O, con este if ya no
     if (element.innerHTML !== "") {
@@ -19,6 +24,11 @@ function cambiarColor(element) {
     } else {
         color = "purple";
         turno = "X"; //cambia el simbolo a X
+    }
+
+    //comprueba si hay un ganador
+    if (comprobarGanador()) {
+        return; //si hay un ganador no permite seguir jugando
     }
 
     if (turno === "O") { //si el turno que toca es O, juega la máquina
@@ -49,6 +59,50 @@ function maquina() {
         color = "purple";
         turno = "X";
     }
+
+    //comprueba si hay un ganador después de que la máquina juega
+    comprobarGanador();
+}
+
+//función para comrpobar si hay un ganador
+function comprobarGanador() {
+    const botones = Array.from(document.querySelectorAll("#tablero button")); //array para guardar las posibles combinaciones para poder ganar el juego
+    const combinaciones = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // filas
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // columnas
+        [0, 4, 8], [2, 4, 6]  // diagonales
+    ];
+
+    let ganar = false;
+
+    combinaciones.forEach(combinacion => {
+        const a = combinacion[0];
+        const b = combinacion[1];
+        const c = combinacion[2];
+
+        if (botones[a].innerHTML !== "" && botones[a].innerHTML === botones[b].innerHTML && botones[a].innerHTML === botones[c].innerHTML) {
+            mostrarMensaje(botones[a].innerHTML);
+            ganar = true; //sí hay un ganador
+        }
+    });
+
+    return ganar; //devuelve si hay un ganador
+}
+
+function mostrarMensaje(resultado) {
+    const mensaje = document.createElement("h3");
+   
+    if (resultado === "X") {
+        mensaje.innerHTML = "¡Has ganado!";
+    } else {
+        mensaje.innerHTML = "Has perdido...";
+    }
+
+    document.getElementById("resultado").innerHTML = ""; //esto elimina el mensaje anterior
+    document.getElementById("resultado").appendChild(mensaje); 
+
+    //cuando se muestra el mensaje, el tablero deja de funcionar
+    tablero = false; 
 }
 
 //función de resetear; es decir, cuando le das al botón de reiniciar desaparecen todos los colores de las casillas
@@ -59,6 +113,10 @@ function reset() {
     });
     color = "purple"; //esto es para que vuelva a comenzar por morado
     turno = "X"; //vuelve a comenzar por X
+    document.getElementById("resultado").innerHTML = ""; //esto elimina el mensaje despues de darle al boton de reset
+
+    //cuando se le da al botón de reset, el tablero vuelve a estar disponible para hacer click en él
+    tablero = true; 
 }
 
 //seleccionamos los botones y los recorremos con el foreach, para que cuando hagamos click cambie el color
